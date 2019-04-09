31ce88e0659b0c49dc67190ad0b23d1e30eb221d
Nathan Pierson <npier2@uic.edu>
2019-04-09 14:48:23 -0500

    Finished day 3

---
+++ Day 3/day3.py
@@ -0,0 +1,46 @@
+with open("input.txt", "r") as f:
+	claims = f.readlines()
+
+for i in range(len(claims)):
+	claims[i] = claims[i].split()
+	del claims[i][1]
+	claim_dict = {}
+	claim_dict["id"] = int(claims[i][0].strip("#"))
+
+	col, row = claims[i][1].strip(":").split(",")
+	claim_dict["col_start"] = int(col)
+	claim_dict["row_start"] = int(row)
+
+	col, row = claims[i][2].split("x")
+	claim_dict["col_end"] = claim_dict["col_start"] + int(col)
+	claim_dict["row_end"] = claim_dict["row_start"]  + int(row)
+
+	claims[i] = claim_dict
+
+print(claims[:10])
+
+loc = "%d, %d" % (5, 30)
+print(loc)
+
+claimants = {}
+for c in claims:
+	for col in range(c["col_start"], c["col_end"]):
+		for row in range(c["row_start"], c["row_end"]):
+			loc = "%d, %d" % (col, row)
+			if loc not in claimants.keys():
+				claimants[loc] = 1
+			else:
+				claimants[loc] += 1
+
+def uncontested(id):
+	for col in range(claims[id]["col_start"], claims[id]["col_end"]):
+		for row in range(claims[id]["row_start"], claims[id]["row_end"]):
+			loc = "%d, %d" % (col, row)
+			if claimants[loc] >= 2:
+				return False
+
+	return True
+
+for c in claims:
+	if uncontested(c["id"] - 1):
+		print(c["id"])
