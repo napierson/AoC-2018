fa7b7d2652ff8fea7f37c19730fbb614c9ce35e8
Nathan Pierson <npier2@uic.edu>
2019-04-09 16:23:57 -0500

    Finished day 5

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,3 +1,5 @@
+import re
+
 with open("input.txt", "r") as f:
 	polymer = f.read()
 
@@ -12,13 +14,31 @@ for i in range(26):
 
 print(pairs)
 
-def unreacted(p):
-	return any(pair in p for pair in pairs)
+def opposite_polarity(a, b):
+	return a != b and (a.upper() == b or a.lower() == b)
 
-while unreacted(polymer):
-	for i in range(len(polymer) - 1):
-		if polymer[i:i+2] in pairs:
-			polymer = polymer[:i] + polymer[i+2:]
-			break
+print(opposite_polarity('b', 'A'))
 
-print(len(polymer))
+def react(p):
+	p_stack = []
+	for u in p:
+		if not p_stack or not opposite_polarity(p_stack[-1], u):
+			p_stack.append(u)
+		else:
+			p_stack.pop()
+	return "".join(p_stack)
+
+test = "abcDEFabc"
+test = re.sub('[ac]', '', test)
+print(test)
+
+reacted = react(polymer)
+print(len(reacted))
+shortest = len(reacted)
+for i in range(26):
+	reduced = re.sub('[%c%c]' % (lowers[i], uppers[i]), '', reacted)
+	reduced = react(reduced)
+	if len(reduced) < shortest:
+		shortest = len(reduced)
+
+print(shortest)
