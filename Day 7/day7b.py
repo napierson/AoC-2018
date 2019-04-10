16a55c390a16614e926d29cca62379f96a9b7388
Nathan Pierson <npier2@uic.edu>
2019-04-10 17:42:56 -0500

    Solved day 7

--- Day 7/day7b.py
+++ Day 7/day7b.py
@@ -21,19 +21,48 @@ for p in prereqs:
 		reverse[after].append(before)
 
 dur = {x:ord(x) - 4 for x in adj.keys()}
-free_at = {x:0 for x in [0, 1, 2, 3, 4]}
+free_at = {x:0 for x in range(5)}
+task_of = {x:None for x in free_at.keys()}
 
-assigned = set()
+pending = set(adj.keys())
+wip = set()
 completed = set()
+
 def available():
-	return sorted([a for a in reverse.keys() if (not reverse[a] and a not in assigned)])
+	return sorted([a for a in pending if not reverse[a]])
+
+def idle():
+	return sorted([m for m in task_of.keys() if task_of[m] == None])
+
+def busy():
+	return sorted([m for m in task_of.keys() if task_of[m] != None])
+
+def advance_time():
+	global time
+	time = min([free_at[m] for m in busy()])
+	for m in busy():
+		if free_at[m] <= time:
+			wip.remove(task_of[m])
+			completed.add(task_of[m])
+			#print("Machine %d completed task %s at time %d" % (m, task_of[m], free_at[m]))
+			for v in adj[task_of[m]]:
+				reverse[v].remove(task_of[m])
+			task_of[m] = None
+
+def assign():
+	global time
+	if available() and idle():
+		todo = available()[0]
+		m = idle()[0]
+		task_of[m] = todo
+		free_at[m] = time + dur[todo]
+		pending.remove(todo)
+		wip.add(todo)
+	else:
+		advance_time()
 
 time = 0
-def assign():
-	if available():
-		todo = available()[0]
-		for worker, avail in free_at.values():
-			if avail <= time:
-				free_at[worker] = time + dur[todo]
-				assigned.add(todo)
-				break
+while pending or wip:
+	assign()
+
+print(free_at)
