6e646d07bb6632a5ad41a8e1f6c03f1f9a19768c
napierson <npiers2@uic.edu>
2019-04-10 07:03:35 -0500

    Progress on 6?

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,8 +1,33 @@
+from scipy import spatial
+
 with open("input.txt", "r") as f:
 	coords = f.readlines()
 
 for i in range(len(coords)):
 	coords[i] = coords[i].strip("\n").split(",")
-	coords[i] = {'x': int(coords[i][0]), 'y': int(coords[i][1])}
+	coords[i] = [int(coords[i][0]), int(coords[i][1])]
 
-print(coords[:10])
+xcoords = sorted(coords, key = lambda c: c[0])
+xmin = xcoords[0][0]
+xmax = xcoords[-1][0]
+
+ycoords = sorted(coords, key = lambda c: c[1])
+ymin = ycoords[0][1]
+ymax = ycoords[-1][1]
+
+print(xmin, xmax, ymin, ymax)
+
+coords_tree = spatial.KDTree(coords)
+nearest = {}
+region_count = {}
+for i in range(len(coords_tree.data)):
+	region_count[str(coords_tree.data[i])] = 0
+
+print(region_count)
+
+for x in range(xmin, xmax + 1):
+	for y in range(ymin, ymax + 1):
+		pass
+		#distances, pts = coords_tree.query([x, y], k = 2, p = 1)
+		#if distances[0] < distances[1]:
+
