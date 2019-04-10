481f5b80e27a3d23b22d0e2a955d0fbbf6544611
Nathan Pierson <npier2@uic.edu>
2019-04-10 14:24:12 -0500

    Not sure if progress

--- Day 7/day7b.py
+++ Day 7/day7b.py
@@ -19,3 +19,21 @@ for p in prereqs:
 		reverse[after] = [before]
 	else:
 		reverse[after].append(before)
+
+dur = {x:ord(x) - 4 for x in adj.keys()}
+free_at = {x:0 for x in [0, 1, 2, 3, 4]}
+
+assigned = set()
+completed = set()
+def available():
+	return sorted([a for a in reverse.keys() if (not reverse[a] and a not in assigned)])
+
+time = 0
+def assign():
+	if available():
+		todo = available()[0]
+		for worker, avail in free_at.values():
+			if avail <= time:
+				free_at[worker] = time + dur[todo]
+				assigned.add(todo)
+				break
