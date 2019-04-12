aaef39fad635e745f2f0b948b3286dd5292a0fe3
Nathan Pierson <npier2@uic.edu>
2019-04-12 11:01:57 -0500

    Finished day 9

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -2,33 +2,44 @@ with open("input.txt", "r") as f:
 	data = f.read()
 	data = data.split()
 	player_ct = int(data[0])
-	marble_max = int(data[6])
+	marble_max = 100 * int(data[6])
 
-marbles = [0]
-# value of the next marble to add
+
+class Marble:
+	def __init__(self, value):
+		self.next = self
+		self.prev = self
+		self.value = value
+
+	def insert(self, m):
+		m.next = self.next
+		m.prev = self
+		m.next.prev = m
+		self.next = m
+
+	def remove(self):
+		self.prev.next = self.next
+		self.next.prev = self.prev
+		return self.next
+
+cur_marble = Marble(0)
 next_marble = 1
-# index of the current marble
-cur_marble = 0
 cur_player = 0
 scores = {p:0 for p in range(player_ct)}
 
-def play_marble():
-	global next_marble, marbles, cur_marble, cur_player, player_ct
-	#print("Initial:", cur_marble, marbles[cur_marble], marbles)
+for i in range(marble_max):
 	if next_marble % 23 != 0:
-		insert_at = ((cur_marble + 1) % len(marbles)) + 1
-		marbles.insert(insert_at, next_marble)
-		#print("After insertion:", cur_marble, marbles[cur_marble], marbles)
-		cur_marble = insert_at
-		#print("New current marble:", cur_marble, marbles[cur_marble], marbles)
+		cur_marble = cur_marble.next
+		cur_marble.insert(Marble(next_marble))
+		cur_marble = cur_marble.next
 	else:
 		scores[cur_player] += next_marble
-		cur_marble = (cur_marble - 7) % len(marbles)
-		scores[cur_player] += marbles.pop(cur_marble)
+		for j in range(7):
+			cur_marble = cur_marble.prev
+		scores[cur_player] += cur_marble.value
+		cur_marble = cur_marble.remove()
+
 	cur_player = (cur_player + 1) % player_ct
 	next_marble += 1
-
-for i in range(marble_max):
-	play_marble()
 
 print(max(scores.values()))
