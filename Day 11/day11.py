78f268bba88c80d0ebfed9555d59423b901c59e7
napierson <npiers2@uic.edu>
2019-04-12 18:22:28 -0500

    Finished day 11

--- Day 11/day11.py
+++ Day 11/day11.py
@@ -1,5 +1,7 @@
 serial = 8772
+
 grid = {}
+
 for x in range(1, 301):
 	for y in range(1, 301):
 		rack_id = x + 10
@@ -12,39 +14,28 @@ for x in range(1, 301):
 		else:
 			power = 0
 		power -= 5
-		grid["%d,%d" % (x, y)] = [power]
-
-def square_power(x, y, n):
-	power = 0
-	for i in range(x, x+n):
-		for j in range(y, y+n):
-			power += grid["%d,%d" % (i, j)]
-
-	return power
+		grid[(x, y)] = {1:power}
 
 max_power = 0
-max_x = 1
-max_y = 1
-max_n = 1
+max_x = 0
+max_y = 0
+max_n = 0
 
-for x in range(1, 300):
-	for y in range(1, 300):
-		for n in range(2, min([25, 301 - x, 301 - y])):
-			next_power = grid["%d,%d" % (x, y)][-1]
-			for i in range(n):
-				next_power += grid["%d,%d" % (x + n, y + i)][0]
-				next_power += grid["%d,%d" % (x + i, y + n)][0]
-			next_power += grid["%d,%d" % (x + n, y + n)][0]
-			grid["%d,%d" % (x, y)].append(next_power)
 
-for x in range(1, 301):
-	for y in range(1, 301):
-		cur_power = max(grid["%d,%d" % (x, y)])
-		cur_n = grid["%d,%d" % (x, y)].index(cur_power) + 1
-		if cur_power > max_power:
-			max_power = cur_power
-			max_x = x
-			max_y = y
-			max_n = cur_n
+for n in range(2, 301):
+	for x in range(1, 302 - n):
+		for y in range(1, 302 - n):
+			if n % 2 == 0:
+				grid[(x, y)][n] = grid[(x, y)][n/2] + grid[(x + n/2, y)][n/2] +\
+								grid[(x, y + n/2)][n/2] + grid[(x + n/2, y + n/2)][n/2]
+			else:
+				grid[(x, y)][n] = grid[(x, y)][n//2 + 1] + grid[(x + n//2 + 1, y)][n//2] +\
+								grid[(x, y + n//2 + 1)][n//2] + grid[(x + n//2, y + n//2)][n//2 + 1] -\
+								grid[(x + n//2, y + n//2)][1]
+			if grid[(x, y)][n] > max_power:
+				max_power = grid[(x, y)][n]
+				max_x = x
+				max_y = y
+				max_n = n
 
-print(max_x, max_y, max_n)
+print(max_x, max_y, max_n, grid[(max_x, max_y)][max_n], max_power)
